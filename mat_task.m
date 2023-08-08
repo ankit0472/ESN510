2e616b01cd9f05b63942f16e5beaa1b5229bc6b6
Ankit moharana <140053319+ankit0472@users.noreply.github.com>
2023-08-08 23:46:50 +0530

    Add files via upload

---
+++ mat_task.m
@@ -0,0 +1,17 @@
+dclc
+a=34;
+b=32;
+c=6;
+d=61;
+e=input("enter the value of e\n");
+f=input("enter the value of f\n");
+g=input("enter the value of g\n");
+h=input("enter the value of h\n");
+w=a+e
+x=b-f
+y=c*g
+z=a/h
+fprintf("%d+%d=%d\n",a,e,w)
+fprintf("%d-%d=%d\n",b,f,x)
+fprintf("%d*%d=%d\n",c,g,y)
+fprintf("%d/%d=%d\n",a,h,z)
